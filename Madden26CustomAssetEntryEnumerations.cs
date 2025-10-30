83eba2899b21427fe6e18a9afea68da2c9c10e0d
Paulov-t <fmt-dev@outlook.com>
2025-10-30 12:48:19 +0000

    Add jersey tab

---
+++ Madden26CustomAssetEntryEnumerations.cs
@@ -0,0 +1,37 @@
+﻿using FMT.PluginInterfaces;
+using FMT.PluginInterfaces.Assets;
+using FMT.ServicesManagers;
+using FMT.ServicesManagers.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Madden26Plugin
+{
+    public class Madden26CustomAssetEntryEnumerations : ICustomAssetEntryEnumerations
+    {
+        private IAssetManagementService assetManagementService => SingletonService.GetInstance<IAssetManagementService>();
+
+        public Dictionary<string, IEnumerable<IAssetEntry>> GetCustomAssetEntriesEnumerations()
+        {
+            var jerseys = assetManagementService
+                .EnumerateEbx()
+                .Where(x => 
+                
+                    x.GetPath().Contains("content/characters/player/parts/uniforms", StringComparison.OrdinalIgnoreCase)
+                    || x.GetPath().Contains("content/characters/player/parts/uniforms/jerseys", StringComparison.OrdinalIgnoreCase)
+                    || x.GetPath().Contains("contentshared/characters/player/parts/uniforms", StringComparison.OrdinalIgnoreCase)
+
+                )
+                .OrderBy(x => x.GetPath())
+                .Select(x => (IAssetEntry)x).ToList();
+
+
+            Dictionary<string, IEnumerable<IAssetEntry>> assetCollections = new();
+            assetCollections.Add("Jersey", jerseys);
+
+
+            return assetCollections;
+        }
+    }
+}
